1985ddf76f3cd8fffb802f58abcfd76dedf31236
inthy <shtern94@gmail.com>
2014-09-20 02:05:38 +0400

    Fucking digital clock
    FUCK CLOCK!!

--- gen/com/shtern/timeupp/R.java
+++ gen/com/shtern/timeupp/R.java
@@ -1091,11 +1091,12 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f05003e;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
+        public static final int clock=0x7f05003d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
@@ -1194,7 +1195,8 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int first_time_screen=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
--- src/com/shtern/timeupp/MainActivity.java
+++ src/com/shtern/timeupp/MainActivity.java
@@ -1,11 +1,15 @@
 package com.shtern.timeupp;
 
 
+
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.DigitalClock;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends ActionBarActivity {
 	ViewPager pager;
@@ -18,6 +22,13 @@ public class MainActivity extends ActionBarActivity {
 		pagerAdapter = new TUPagerAdapter();
 		pager.setOffscreenPageLimit(7);
 		pager.setAdapter(pagerAdapter);
+		RelativeLayout ttpage = (RelativeLayout) getLayoutInflater().inflate(
+				R.layout.first_time_screen, null);
+		
+		CustomDigitalClock clock = (CustomDigitalClock)ttpage.findViewById(R.id.clock);
+		clock.setTypeface(Typeface.createFromAsset(getAssets(),
+				"digital-7.ttf"));
+		pagerAdapter.addView(ttpage);
 	}
 
 
